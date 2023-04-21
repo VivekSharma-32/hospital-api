9e46f4c1e3421e96d7200f06a34b8f54d3c4bd2b
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-04-21 18:05:41 +0530

    first commit

--- config/configdb.js
+++ config/configdb.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
- async function main(){
-    await mongoose.connect('mongodb+srv://hospital:z2swpgr4Ken6sdGz@cluster1.c3wrh.mongodb.net/hospitalApi?retryWrites=true&w=majority');
-    console.log("Connection SuccessFull !!");
+const mongoose = require("mongoose");
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  console.log("Connection SuccessFull !!");
 }
-main().catch((error) =>{console.log("Connection Not Success Full !!");})
+main().catch((error) => {
+  console.log("Connection Not Success Full !!");
+});
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 8000;
 const db = require("./config/configdb");
-const Doctor = require('./models/doctor');
+const Doctor = require("./models/doctor");
+const dotenv = require("dotenv");
+
 const app = express();
+
+dotenv.config();
+
 app.use(express.urlencoded());
 app.use(express.json());
-app.use('/', require('./router/index'));
+
+app.use("/", require("./router/index"));
+
 app.listen(port, (error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is running");
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is running");
+  }
 });
